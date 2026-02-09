89889a98781dde02ab960a7da7d5c217d4a9fb3c
Sharon-DA <sharonakumba@gmail.com>
2026-02-09 16:42:51 +0100

    Update Safe Schools Tool label

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ const modules = [
     href: ''
   },
   {
-    title: 'School Tracker',
+    title: 'Safre Schools Tool',
     subtitle: 'Monitor School Performance',
     icon: '🏫📍',
     href: ''
